9a4ebe3220a193151389e2014cf21f336c05d1e4
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-17 19:35:28 -0500

    render text w/ link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
 const node = document.getElementById("root");
+const root = React.createElement(
+  "div",
+  {},
+  React.createElement(
+    "h1",
+    {},
+    "Hello, world!",
+    React.createElement(
+      "a",
+      { href: "mailto:keenanrc@hotmail.com" },
+      React.createElement("h1", {}, "React In Action"),
+      React.createElement("em", {}, "...and now it really is!")
+    )
+  )
+);
+
+render(root, node);
