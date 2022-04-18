4ad0b69f1fab90505c4f740d23ff10b42629540d
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-17 19:57:01 -0500

    reverse-engineer component tree

--- src/index.js
+++ src/index.js
@@ -1,46 +1,105 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 const node = document.getElementById("root");
 
 class Post extends Component {
-    render() {
-        return React.createElement(
-            'div',
-            {
-                className: 'post'
-            },
-            React.createElement(
-                'h2',
-                {
-                    className: 'postAuthor',
-                    id: this.props.id
-                },
-                this.props.user,
-                React.createElement(
-                    'span',
-                    {
-                        className: 'postBody'
-                    },
-                    this.props.content
-                )
-            )
-        );
-    }
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "post",
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "postAuthor",
+          id: this.props.id,
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "postBody",
+          },
+          this.props.content
+        ),
+        this.props.children
+      )
+    );
+  }
 }
 
 Post.propTypes = {
-    user: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
-const App = React.createElement(Post, {
-    id: 1,
-    content: ' said: This is a post!',
-    user: 'mark'
-});
+class Comment extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "comment",
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "commentAuthor",
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "commentContent",
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+};
+
+class Box extends Component {
+    render() {
+        return React.createElement(
+            "div",
+            {
+                className: "Box"
+            }
+        )
+    }
+}
+
+const App = React.createElement(
+    Box,
+    {
+        id: 1,
+        user: 'patrick',
+    }
+)
+
+// const App = React.createElement(
+//   Post,
+//   {
+//     id: 1,
+//     content: " said: This is a post!",
+//     user: "mark",
+//   },
+//   React.createElement(Comment, {
+//     id: 2,
+//     user: "bob",
+//     content: " commented: wow! how cool!",
+//   })
+// );
 
 render(App, node);
 
