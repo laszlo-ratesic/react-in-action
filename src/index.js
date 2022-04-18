3bc9c719debc0d8281755c880cdefbbf8af71021
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-04-17 19:43:42 -0500

    render post

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import PropTypes from "prop-types"
 
 const node = document.getElementById("root");
-const root = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "h1",
-    {},
-    "Hello, world!",
-    React.createElement(
-      "a",
-      { href: "mailto:keenanrc@hotmail.com" },
-      React.createElement("h1", {}, "React In Action"),
-      React.createElement("em", {}, "...and now it really is!")
-    )
-  )
-);
 
-render(root, node);
+class Post extends Component {
+    render() {
+        return React.createElement(
+            'div',
+            {
+                className: 'post'
+            },
+            React.createElement(
+                'h2',
+                {
+                    className: 'postAuthor',
+                    id: this.props.id
+                },
+                this.props.user,
+                React.createElement(
+                    'span',
+                    {
+                        className: 'postBody'
+                    },
+                    this.props.content
+                )
+            )
+        );
+    }
+}
+
+Post.propTypes = {
+    user: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired
+};
+
+const App = React.createElement(Post, {
+    id: 1,
+    content: ' said: This is a post!',
+    user: 'mark'
+});
+
+render(App, node);
+
+// const root = React.createElement(
+//   "div",
+//   {},
+//   React.createElement(
+//     "h1",
+//     {},
+//     "Hello, world!",
+//     React.createElement(
+//       "a",
+//       { href: "mailto:keenanrc@hotmail.com" },
+//       React.createElement("h1", {}, "React In Action"),
+//       React.createElement("em", {}, "...and now it really is!")
+//     )
+//   )
+// );
+
+// render(root, node);
